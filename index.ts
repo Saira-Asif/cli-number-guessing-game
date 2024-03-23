7c2bcc7d9eb26f651e35be61744da5c2c637b4f8
Saira-Asif <160786613+Saira-Asif@users.noreply.github.com>
2024-03-24 04:11:05 +0500

    my first commit

---
+++ index.ts
@@ -0,0 +1,25 @@
+#!  /usr/bin/env node
+
+import inquirer from "inquirer";
+
+//? 1) Computer will generate a general number. -done
+//? 2) User Input for guessing number. -done
+//? 3) Compare user input with computer generated number and show result.
+
+
+const randomNumber = 24;
+
+const Answer = await inquirer.prompt([{
+ 
+    name: "UserGuessedNumber",
+    type: "number",
+    message:"Please Guess your Number."
+}])
+
+if (Answer.UserGuessedNumber === randomNumber) {
+
+    console.log("Congrats! You guessed the right number.")
+} else {
+
+    console.log("You guessed the wrong number.")
+}
